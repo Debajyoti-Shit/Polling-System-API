e77044dddc4f67f8ce13b7f3a0dd4b15daa81190
Debajyoti <76768317+Debajyoti-Shit@users.noreply.github.com>
2023-04-03 22:24:26 +0530

    Update moongose.js

--- config/moongose.js
+++ config/moongose.js
@@ -1,5 +1,4 @@
 // require the library
-require('dotenv').config()
 const mongoose = require("mongoose");
 const db_url ="mongodb+srv://debajyotishit3:Deba16@cluster-polling-api.nawjv52.mongodb.net/?retryWrites=true&w=majority";
 
